ccef33b4d179d5dff4badd117c27f973f56aa17c
David Bernhard <dbernhard@ethz.ch>
2023-08-23 00:32:22 +0200

    Quiet execution of ExtractArticle.js

--- readabilipy/simple_json.py
+++ readabilipy/simple_json.py
@@ -53,7 +53,11 @@ def simple_json_from_html_string(html, content_digests=False, node_indexes=False
         article_json_path = f_html.name + ".json"
         jsdir = os.path.join(os.path.dirname(__file__), 'javascript')
         subprocess.check_call(
-            ["node", "ExtractArticle.js", "-i", html_path, "-o", article_json_path], cwd=jsdir)
+            ["node", "ExtractArticle.js", "-i", html_path, "-o", article_json_path, cwd=jsdir],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
         # Read output of call to Readability.parse() from JSON file and return as Python dictionary
         with open(article_json_path, "r", encoding="utf-8") as json_file:
